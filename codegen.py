bfc5c7695aabe4e79ca939909a20a05d3f4ec1dd
Steven Freed <saf236@scarletmail.rutgers.edu>
2020-08-28 11:22:49 -0400

    Call node implemented, kwarg/arg issues, need to fix string args to have quotes

--- codegen.py
+++ codegen.py
@@ -1,37 +1,58 @@
 import ast
 import decimal
 
-def NotInPrint(node, _nodemap):
+def CallPrint(node, nodemap):
+    func = generate_code(node.func)
+    callstr = f"{func}("
+    for arg in node.args:
+        arg = generate_code(arg)
+        callstr = f"{callstr}{arg},"
+    kwargdict = "{"
+    for kwarg in node.keywords:
+        kwarg = generate_code(kwarg)
+        kwargdict = f"{kwargdict}{kwarg},"
+    if kwargdict != "{":
+        kwargdict = f"{kwargdict[:-1]}}}"
+        callstr = f"{callstr}{kwargdict});"
+    else:
+        callstr = f"{callstr[:-1]});"
+    return callstr
+
+def keywordPrint(node, nodemap):
+    value = generate_code(node.value)
+    return f"'{node.arg}':{value}"
+
+def NotInPrint(node, nodemap):
     return None
 
-def InPrint(node, _nodemap):
+def InPrint(node, nodemap):
     return "in"
 
-def IsNotPrint(node, _nodemap):
+def IsNotPrint(node, nodemap):
     return "!=="
 
-def IsPrint(node, _nodemap):
+def IsPrint(node, nodemap):
     return "==="
 
-def GtEPrint(node, _nodemap):
+def GtEPrint(node, nodemap):
     return ">="
 
-def GtPrint(node, _nodemap):
+def GtPrint(node, nodemap):
     return ">"
 
-def LtEPrint(node, _nodemap):
+def LtEPrint(node, nodemap):
     return "<="
 
-def LtPrint(node, _nodemap):
+def LtPrint(node, nodemap):
     return "<"
 
-def NotEqPrint(node, _nodemap):
+def NotEqPrint(node, nodemap):
     return "!="
 
-def EqPrint(node, _nodemap):
+def EqPrint(node, nodemap):
     return "=="
 
-def ComparePrint(node, _nodemap):
+def ComparePrint(node, nodemap):
     left = generate_code(node.left)
     cmpstr = str(left)
     for i in range(len(node.comparators)):
@@ -48,46 +69,46 @@ def ComparePrint(node, _nodemap):
             cmpstr = f"{cmpstr} {op} {comp}"
     return cmpstr
 
-def AddPrint(node, _nodemap):
+def AddPrint(node, nodemap):
     return "+"
 
-def SubPrint(node, _nodemap):
+def SubPrint(node, nodemap):
     return "-"
 
-def MultPrint(node, _nodemap):
+def MultPrint(node, nodemap):
     return "*"
 
-def DivPrint(node, _nodemap):
+def DivPrint(node, nodemap):
     return "/"
 
-def FloorDivPrint(node, _nodemap):
+def FloorDivPrint(node, nodemap):
     return "/"
 
-def ModPrint(node, _nodemap):
+def ModPrint(node, nodemap):
     return "%"
 
-def PowPrint(node, _nodemap):
+def PowPrint(node, nodemap):
     return None
 
-def LShiftPrint(node, _nodemap):
+def LShiftPrint(node, nodemap):
     return "<<"
 
-def RShiftPrint(node, _nodemap):
+def RShiftPrint(node, nodemap):
     return ">>"
 
-def BitOrPrint(node, _nodemap):
+def BitOrPrint(node, nodemap):
     return "|"
 
-def BitXorPrint(node, _nodemap):
+def BitXorPrint(node, nodemap):
     return "^"
 
-def BitAndPrint(node, _nodemap):
+def BitAndPrint(node, nodemap):
     return "&"
 
-def MatMultPrint(node, _nodemap):
+def MatMultPrint(node, nodemap):
     raise SyntaxError("Compiler does not support Matrix Multiplication Binary Operator '@'")
 
-def BinOpPrint(node, _nodemap):
+def BinOpPrint(node, nodemap):
     left, op, right = generate_code(node.left), generate_code(node.op), generate_code(node.right)
     if isinstance(node.op, ast.Pow):
         return f"Math.pow({left}, {right})"
@@ -96,47 +117,47 @@ def BinOpPrint(node, _nodemap):
     else:
         return f"{left}{op}{right}"
 
-def NotPrint(node, _nodemap):
+def NotPrint(node, nodemap):
     return "!"
 
-def UnaryOpPrint(node, _nodemap):
+def UnaryOpPrint(node, nodemap):
     return f"{generate_code(node.op)}{generate_code(node.operand)}"
 
-def BoolOpPrint(node, _nodemap):
+def BoolOpPrint(node, nodemap):
     left, right = [generate_code(node) for node in node.values]
     bool_ = generate_code(node.op)
     return f"{left} {bool_} {right}"
 
-def OrPrint(node, _nodemap):
+def OrPrint(node, nodemap):
     return "||"
 
-def AndPrint(node, _nodemap):
+def AndPrint(node, nodemap):
     return "&&"
 
-def DictPrint(node, _nodemap):
+def DictPrint(node, nodemap):
     keys = [generate_code(el) for el in node.keys]
     values = [generate_code(el) for el in node.values]
     return dict(zip(keys, values))
 
-def SetPrint(node, _nodemap):
+def SetPrint(node, nodemap):
     elts = ",".join(set(generate_code(el) for el in node.elts))
     return f"[{elts}]"
 
-def ListPrint(node, _nodemap):
+def ListPrint(node, nodemap):
     elts = ",".join([generate_code(el) for el in node.elts])
     return f"[{elts}]"
 
-def TuplePrint(node, _nodemap):
+def TuplePrint(node, nodemap):
     elts = ",".join([str(generate_code(el)) for el in node.elts])
     return f"[{elts}]"
 
-def NamePrint(node, _nodemap):
+def NamePrint(node, nodemap):
     return node.id
 
-def FormattedValuePrint(node, _nodemap):
+def FormattedValuePrint(node, nodemap):
     return generate_code(node.value)
 
-def ConstantPrint(node, _nodemap):
+def ConstantPrint(node, nodemap):
     numeric = (float, int, decimal.Decimal,)
     string = (str,)
     if type(node.value) in numeric:
@@ -147,7 +168,7 @@ def ConstantPrint(node, _nodemap):
         nameconstmap = {True: "true", False: "false", None: "null"}
         return nameconstmap[node.value]
 
-def JoinedStrPrint(node, _nodemap):
+def JoinedStrPrint(node, nodemap):
     strval = ''
     for n in node.values:
         token = generate_code(n)
@@ -156,13 +177,13 @@ def JoinedStrPrint(node, _nodemap):
         strval = strval[:-1]
     return strval
 
-def FunctionDefPrint(node, _nodemap):
+def FunctionDefPrint(node, nodemap):
     keyword = "function"
     name = node.name
     args = ','.join([arg.arg for arg in node.args.args])
     return f"{keyword} {name}({args}){{"
 
-def AssignPrint(node, _nodemap):
+def AssignPrint(node, nodemap):
     declare = "var"
     targets = ",".join([generate_code(target) for target in node.targets])
     value = generate_code(node.value)
@@ -209,13 +230,15 @@ _nodemap = {
     ast.IsNot: IsNotPrint,
     ast.In: InPrint,
     ast.NotIn: NotInPrint,
+    ast.Call: CallPrint,
+    ast.keyword: keywordPrint,
 }
 def generate_code(node):
     try:
         return _nodemap[type(node)](node, _nodemap)
     except KeyError:
         if isinstance(node, ast.Expr):
-            return generate_code(node.value)
+            return _nodemap[type(node.value)](node.value, _nodemap)
         else:
             return ''.join([generate_code(child) for child in node.body])
 
